a3ff211cf70f1b32072ecc2192ba5f5287810860
Ashwin A <119737155+Ashwinmic@users.noreply.github.com>
2024-02-23 21:40:09 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package org.first;
+
+public class Employee {
+ public void empId() {
+	 System.out.println("Employee id is 555");
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee e=new Employee();
+e.empId();
+	}
+
+}
